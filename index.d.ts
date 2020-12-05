1511d698835c7f8b0c7db6e231973c4c41b3e7b3
egy186 <egy186@yahoo.co.jp>
2020-12-05 14:03:03 +0900

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,48 @@
+/// <reference types="node" />
+
+import { EventEmitter } from 'events';
+
+export interface Code {
+  [key: number]: number;
+}
+
+export interface AverageOptions {
+  tolerance?: number;
+}
+
+export function average(codes: Code[], options?: AverageOptions): Code;
+
+export interface ListenOptions {
+  maxWidth?: number;
+  minWidth?: number;
+}
+
+export class Listener extends EventEmitter {
+  addListener(eventName: 'data', listener: (code: Code) => void | Promise<void>): this;
+  listeners(eventName: 'data'): ((code: Code) => void | Promise<void>)[];
+  off(eventName: 'data', listener: (code: Code) => void | Promise<void>): this;
+  on(eventName: 'data', listener: (code: Code) => void | Promise<void>): this;
+  once(eventName: 'data', listener: (code: Code) => void | Promise<void>): this;
+  prependListener(eventName: 'data', listener: (code: Code) => void | Promise<void>): this;
+  prependOnceListener(eventName: 'data', listener: (code: Code) => void | Promise<void>): this;
+  removeListener(eventName: 'data', listener: (code: Code) => void | Promise<void>): this;
+  rawListeners(eventName: 'data'): ((code: Code) => void | Promise<void>)[];
+}
+
+export function listen(gpio: Number, options?: ListenOptions): Listener;
+
+export interface RecordOptions {
+  averageOptions?: AverageOptions;
+  confirm?: number;
+  listenOptions?: ListenOptions;
+  minLength?: number;
+}
+
+export function record(gpio: Number, options?: RecordOptions): Promise<Code>;
+
+export interface SendOptions {
+  frequency?: number;
+  interval?: number;
+}
+
+export function send(gpio: Number, codeOrCodes: Code | Code[], options?: SendOptions): Promise<void>;
